85c067a5b3d5d957c1d011482205f523f1f1d8d5
jamaledim <83904130+jamaledim@users.noreply.github.com>
2023-07-27 23:30:50 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -88,6 +88,9 @@ cd -
 
 gem install wpscan
 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install ./google-chrome-stable_current_amd64.deb
+rm -rf google-chrome-stable_current_amd64.deb
 
 
 
